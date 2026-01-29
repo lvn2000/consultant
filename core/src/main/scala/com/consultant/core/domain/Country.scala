cc516ae12a8bcc05c9757d8b731a6c04bb001780
Volodymyr Lubenchenko <vlad.lubenchenko@gmail.com>
2026-01-29 12:22:31 +0200

    Countries (schema and data))

--- core/src/main/scala/com/consultant/core/domain/Country.scala
+++ core/src/main/scala/com/consultant/core/domain/Country.scala
@@ -5,13 +5,14 @@ import types.*
 
 case class Country(
   id: CountryId,
-  code: String, // e.g., "US", "RU"
-  name: String  // e.g., "United States", "Russia"
+  code: String,     // e.g., "US", "RU"
+  name: String,     // e.g., "United States", "Russia"
+  shortname: String // e.g., "USA", "Russia"
 )
 
 object Country {
-  def apply(code: String, name: String): Country =
-    Country(UUID.randomUUID(), code, name)
+  def apply(code: String, name: String, shortname: String): Country =
+    Country(UUID.randomUUID(), code, name, shortname)
 }
 
 type CountryId = UUID
